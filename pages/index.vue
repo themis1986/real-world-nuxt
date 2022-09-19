e68c2088fc12272715a928e6b518e41970663029
thiakovidis <thiakovidis@agroapps.gr>
2022-09-19 17:12:46 +0300

    File based routing and api calls with axios

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,30 @@
 <template>
-  <h1>Events</h1>
+  <div class="">
+    <h1>Events</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
 </template>
 
 <script>
+import EventCard from "~/components/EventCard.vue";
 export default {
   name: "IndexPage",
+  components: { EventCard },
+  asyncData({ $axios, error }) {
+    return $axios
+      .get("http://localhost:3001/events")
+      .then((res) => {
+        return {
+          events: res.data,
+        };
+      })
+      .catch((e) => {
+        error({
+          statusCode: 503,
+          message: "Unable to fetch events at this time. Please try again later.",
+        });
+      });
+  },
   head() {
     return {
       title: "Event Listing",
